5d5de0047cb5db0327dc73fac96b5395accc6f7e
Luis <luisg9410@gmail.com>
2017-09-23 15:29:10 -0500

    Auth reducer

--- reducers/index.js
+++ reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux'
+import auth from './auth_reducer'
 
-export default combineReducers({
-  auth: () => { return {} }
-})
+export default combineReducers({ auth })
