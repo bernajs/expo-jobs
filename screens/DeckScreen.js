291dc7df67a838e4b1ba6b15219df2199961065f
Luis García <luisgarcia@Luiss-MacBook-Pro.local>
2017-10-04 11:59:18 -0500

    Add: Swiper Card

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Platform } from 'react-native'
 import { connect } from 'react-redux'
 import Swipe from '../components/Swipe'
 import { MapView } from 'expo'
@@ -7,17 +7,36 @@ import { Button, Card } from 'react-native-elements'
 
 
 class DeckScreen extends Component {
-  renderCards(job){
+  renderCard(job){
+const initialRegion = {
+  latitude: job.latitude,
+  longitude: job.longitude,
+  latitudeDelta: 0.045,
+  longitudeDelta: 0.02
+}
+
     return(
       <Card title={job.jobtitle}>
+        <View style={{height: 300}}>
+          <MapView scrollEnabled={false}
+            style={{flex: 1}}
+            initialRegion={initialRegion}
+            cacheEnabled={Platform.OS === 'android' ? true : false}></MapView>
+        </View>
         <View style={styles.detailWrapper}>
           <Text>{job.company}</Text>
           <Text>{job.formattedRelativeTime}</Text>
         </View>
         <Text>
-          {job.snippet.replace(/<b>/g, '').replace(/</b>/g, '')}
+          {job.snippet.replace(/<b>/g, '').replace(/<\/b>/g, '')}
         </Text>
       </Card>
+    )
+  }
+
+  renderNoMoreCards(){
+    return(
+      <Card title="No more jobs"></Card>
     )
   }
   render() {
@@ -25,7 +44,8 @@ class DeckScreen extends Component {
       <View>
         <Swipe
           data={this.props.jobs}
-          renderCard={this.renderCard}></Swipe>
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}></Swipe>
       </View>
     )
   }
